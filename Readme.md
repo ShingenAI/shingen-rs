2bd599cd7060911425794adb4e7713f4386febbf
Eese <eese@sakuraux.com>
2025-09-13 19:13:50 +0900

    ignite mod

--- Readme.md
+++ Readme.md
@@ -1,1 +1,12 @@
-Living Recursion Dictionary
+Living Recursion:
+
+Rust Toolchain
+- rustc (the compiler)
+- cargo (the ritual orchestrator)
+- rust-std (the standard library)
+- and keeps everything updated
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+cargo new shingen-rs --vcs none
+cd shingen-rs
---
+++ shingen-rs/target/debug/deps/shingen_rs-7beb9b1d806a7992.d
@@ -0,0 +1,7 @@
+/Users/eesekashi/DEV/Shingen-AI/Shingen-rs/shingen-dimonds-and-rust/shingen-rs/target/debug/deps/shingen_rs-7beb9b1d806a7992.d: src/main.rs src/rituals/mod.rs src/rituals/ignite.rs
+
+/Users/eesekashi/DEV/Shingen-AI/Shingen-rs/shingen-dimonds-and-rust/shingen-rs/target/debug/deps/shingen_rs-7beb9b1d806a7992: src/main.rs src/rituals/mod.rs src/rituals/ignite.rs
+
+src/main.rs:
+src/rituals/mod.rs:
+src/rituals/ignite.rs:
